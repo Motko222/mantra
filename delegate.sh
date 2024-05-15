92f17a89553db9aee9155c24ba89b8ba286d3937
Motko <77466238+Motko222@users.noreply.github.com>
2024-05-15 11:18:57 +0200

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -5,8 +5,6 @@
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 source ~/scripts/$folder/cfg
 
-
-
 [ -z $1 ] && read -p "From ($KEY) ? " key || key=$1
 [ -z $key ] && key=$KEY
 
