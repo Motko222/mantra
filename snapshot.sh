353b6a507cc6ba42bbd781390001e141216dee55
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-28 12:19:12 +0200

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -3,7 +3,7 @@
 sudo systemctl stop mantrachaind
 
 # Back up priv_validator_state.json if needed
-cp ~/.mantrachain/data/priv_validator_state.json  ~/.mantrachaind/priv_validator_state.json
+cp ~/.mantrachain/data/priv_validator_state.json  ~/.mantrachain/priv_validator_state.json
 
 cd $HOME
 sudo rm -rf ~/.mantrachain/data
@@ -13,7 +13,7 @@ curl -o - -L https://config-t.noders.services/mantra/data.tar.lz4 | lz4 -d | tar
 curl -o - -L https://config-t.noders.services/mantra/wasm.tar.lz4 | lz4 -d | tar -x -C ~/.mantrachain
 
 # Replace with the backed-up priv_validator_state.json
-cp ~/.mantrachain/priv_validator_state.json  ~/.mantrachaind/data/priv_validator_state.json
+cp ~/.mantrachain/priv_validator_state.json  ~/.mantrachain/data/priv_validator_state.json
 
 sudo systemctl restart mantrachaind
 sudo journalctl -fu mantrachaind --no-hostname -o cat
